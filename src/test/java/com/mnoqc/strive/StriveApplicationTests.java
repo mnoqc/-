0a9e5c1147ab6738027868d09ab80daff6269ee4
mnoqc <nmomnr@gmail.com>
2024-05-22 18:14:58 +0900

    initial commit #2

---
+++ src/test/java/com/mnoqc/strive/StriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnoqc.strive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
